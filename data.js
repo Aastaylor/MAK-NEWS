d1f9627c5092e7a0cfd519421955645c10b84710
Aastaylor <aasthag2108@gmail.com>
2025-03-22 14:30:25 +0530

    Deploying to GitHub Pages

---
+++ data.js
@@ -0,0 +1,1 @@
+export const API_KEY = "269a2552667748ee9a50a3aa88c5c0f7";
--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -1,20 +1,35 @@
 import { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
 import "./NewsBoard.css"
+import { API_KEY } from '../../data'
 
 const NewsBoard = ({category}) => {
 
     const [articles, setArticles] = useState([]);
 
     useEffect(()=>{
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+      // fetch('https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}', {
+      //   method: 'GET',
+      //   headers: {
+      //     'Accept': 'application/json',
+      //     'Upgrade-Insecure-Requests': '1'
+      //   }
+      // })
+      //   .then(response => {
+      //     if (!response.ok) throw new Error(`HTTP Error: ${response.status}`);
+      //     return response.json();
+      //   })
+      //   then(data=> setArticles(data.articles))
+      //   .catch(error => console.error('Error:', error));
+      
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`;
         fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
 
     },[category])
   return (
     <div className="inner-content">
       <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
-      {articles.map((news,index)=>{
+      {articles?.map((news,index)=>{
         return <NewsItem key ={index} news={news}/>
       })}
     </div>
